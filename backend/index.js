1f4c66c0c239211ae798e679143e0e8c9fceda11
qcc42 <qcc.1992@gmail.com>
2024-10-07 14:59:33 +0300

    Saving code from tutorial

--- backend/index.js
+++ backend/index.js
@@ -1,58 +1,48 @@
+
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
+require('dotenv').config()
+const Note = require('./models/note')
+var jsonParser = require('body-parser').json()
 
-let persons = [
-
-  {
-    name: "Jorris",
-    number: 1123
-  }
-]
-
-app.use(express.static('dist'))
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-const cors = require('cors')
-app.use(express.static('dist'))
-app.use(express.json())
-app.use(requestLogger)
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-
-app.post('/api/persons', (request, response) => {
-  const body = request.body
-
-
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  persons = persons.concat(person)
-
-  response.json(person)
-})
-
-app.use(unknownEndpoint)
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+app.post('/api/notes', jsonParser, (request, response) => {
+  const body = request.body
+  console.log(request)
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+
+})
+
+  app.get('/api/notes/:id', (request, response) => {
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
+  })
+
+
+
+
+
