ee678d3f14ea0b8bb501b00b8852a9f91fffe6ea
qcc42 <qcc.1992@gmail.com>
2024-10-25 11:59:18 +0300

    Tutoruial code up-to-date until assignemtn 4.1.

--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,8 @@
+const app = require('./app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const mongoose = require('mongoose')
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const Note = require('./models/note')
-var jsonParser = require('body-parser').json()
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
-
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
-
-app.post('/api/notes', jsonParser, (request, response) => {
-  const body = request.body
-  console.log(request)
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
-
-})
-
-  app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-      response.json(note)
-    })
-  })
-
-
-
-
 
